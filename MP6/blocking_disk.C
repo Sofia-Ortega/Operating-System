bc6b958f2331d1ca4c43cb2c7dd447ab1c437cbf
Sofia Ortega <s.ortega@tamu.edu>
2023-04-07 09:39:30 -0500

    added push and pop queue in BlockingDisk

--- MP6/blocking_disk.C
+++ MP6/blocking_disk.C
@@ -22,13 +22,45 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "machine.H"
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
+BlockingDisk* BlockingDisk::blockedQueue = nullptr;
+
+void BlockingDisk::push_queue() {
+  if (blockedQueue == nullptr) {
+    blockedQueue = this;
+  } else {
+    // push to end of queue 
+    tail->next = this; 
+    tail = this;
+  }
+}
+
+BlockingDisk* BlockingDisk::pop_queue() {
+  assert(blockedQueue != nullptr); // make sure SOMETHING in queue
+
+  // is head of blocked queue ready?
+    // yes -> pop from queue and return
+    // no -> return nullptr (fail)
+  
+  BlockingDisk* disk = blockedQueue;
+  if(disk->is_ready()) {
+    // pop from queue
+    blockedQueue = blockedQueue->next;
+    return disk;
+  } 
+
+
+  
+}
 
 BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size) {
+    next = nullptr;
+    
 }
 
 /*--------------------------------------------------------------------------*/
@@ -37,6 +69,25 @@ BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
 
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
   // -- REPLACE THIS!!!
+
+  // issue_operation(DISK_OPERATION::READ, _block_no);
+
+
+
+
+
+  // bad
+  while (!is_ready()) { /* wait */; }
+
+  /* read data from port */
+  int i;
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++) {
+    tmpw = Machine::inportw(0x1F0);
+    _buf[i*2]   = (unsigned char)tmpw;
+    _buf[i*2+1] = (unsigned char)(tmpw >> 8);
+  }
+
   SimpleDisk::read(_block_no, _buf);
 
 }
